9ba764ad238da3d491f739c5ee494e81567b531f
Nelson Scatarelli <nelson.scatarelli@flugel.it>
2021-07-18 18:11:42 -0300

    wait for worker node

--- kubernetes-basics/core-concepts/env-init.sh
+++ kubernetes-basics/core-concepts/env-init.sh
@@ -22,3 +22,18 @@ source <(helm completion bash)
 helm install metrics-server stable/metrics-server --namespace kube-system 
 
 { clear && echo 'Kubernetes with Helm is ready.'; } 2> /dev/null
+
+# Wait for the worker node to be available
+i=1
+ready=0
+while [ "$ready" -eq 0 ]; do
+ ready=`kubectl get nodes | grep node01 | wc -l`
+ echo "waiting for worker node ..."
+ sleep 10
+
+ # timeout ..
+ let "i=i+1"
+ if [ "i" -gt 10 ];
+   ready=1
+ fi
+done
